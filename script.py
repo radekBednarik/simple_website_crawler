524d954d86c576d2b6057beccb2268ac57a19b67
radekBednarik <radek.bednarik@tesena.com>
2020-06-01 18:41:58 +0200

    bugfix - now should scan all internal links

--- script.py
+++ script.py
@@ -9,9 +9,9 @@ HOSTNAME = "https://svse-v2-sint.edge-sint.k2ng-dev.net"
 s = r.Session()
 
 
-def cook_soup(url, index):
+def cook_soup(url):
     response = s.get(url, timeout=(60, 120))
-    print(f"URL no. {index}: {url} : response status --> {response.status_code}")
+    print(f"URL: {url} : response status --> {response.status_code}")
     return BeautifulSoup(response.text, "lxml")
 
 
@@ -30,8 +30,8 @@ def create_full_link(hostname, internal_link):
     return urljoin(hostname, internal_link)
 
 
-def process_page(url, index):
-    links = get_internal_links(cook_soup(url, index))
+def process_page(url):
+    links = get_internal_links(cook_soup(url))
     full_links = {create_full_link(HOSTNAME, link) for link in links}
     return full_links
 
@@ -46,19 +46,20 @@ def extend_links_to_visit(new_links, links_to_visit, visited):
 
 # pylint:disable=dangerous-default-value
 def looper(visited=set(), links_to_visit=None):
-    links_to_visit = process_page(HOSTNAME, 0)
+    links_to_visit = process_page(HOSTNAME)
 
     while True:
         if visited != links_to_visit:
-            for index, link in enumerate(list(links_to_visit), 1):
+            for link in list(links_to_visit):
                 if link not in visited:
                     visited.add(link)
                     links_to_visit = extend_links_to_visit(
-                        process_page(link, index), links_to_visit, visited
+                        process_page(link), links_to_visit, visited
                     )
                 else:
                     continue
-        break
+        else:
+            break
     return visited
 
 
